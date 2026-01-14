1586c53133c710ed4ee4cf174182683ee7e3bb6f
Aleksandar Djukic <djukicaleks91@gmail.com>
2026-01-14 23:48:55 +0100

    feat: add Product, Cart, CartItem, Order, OrderItem models with migrations and relations

---
+++ app/Models/Order.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $fillable = ['user_id', 'total_price'];
+
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+}
+
